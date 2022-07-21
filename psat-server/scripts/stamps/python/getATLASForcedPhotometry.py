ed625370d082be528d470e6354368e870c91923a
Ken Smith <k.w.smith@qub.ac.uk>
2022-07-21 02:10:26 +0100

    Added first drafts of getATLASForcedPhotometry.py and getATLASForcedPhotometryMultiprocess.py

---
+++ psat-server/scripts/stamps/python/getATLASForcedPhotometry.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python
+"""Do ATLAS forced photometry.
+
+Usage:
+  %s <configfile> [<candidate>...] [--detectionlist=<detectionlist>] [--customlist=<customlist>] [--limit=<limit>] [--update] [--ddc] [--skipdownload] [--redregex=<redregex>] [--diffregex=<diffregex>] [--redlocation=<redlocation>] [--difflocation=<difflocation>] [--tphorce=<tphorcelocation>] [--useflagdate]
+  %s (-h | --help)
+  %s --version
+
+Options:
+  -h --help                                   Show this screen.
+  --version                                   Show version.
+  --update                                    Update the database
+  --detectionlist=<detectionlist>             List option
+  --customlist=<customlist>                   Custom List option
+  --limit=<limit>                             Number of detections for which to request images [default: 6]
+  --ddc                                       Use the DDC schema for queries
+  --skipdownload                              Do not attempt to download the exposures (assumes they already exist locally)
+  --redregex=<redregex>                       Reduced image regular expression. Caps = variable. [default: EXPNAME.fits.fz]
+  --diffregex=<diffregex>                     Diff image regular expression. Caps = variable. [default: EXPNAME.diff.fz]
+  --redlocation=<redlocation>                 Reduced image location. E.g. /atlas/diff/CAMERA/fake/MJD.fake (caps = special variable).  Null value means use standard ATLAS archive location.
+  --difflocation=<difflocation>               Diff image location. E.g. /atlas/diff/CAMERA/fake/MJD.fake (caps = special variable). Null value means use standard ATLAS archive location.
+  --tphorce=<tphorcelocation>                 Location of the tphorce shell script [default: /usr/local/ps1code/gitrelease/tphorce/tphorce].
+  --useflagdate                               Use the flag date as the threshold for the number of days instead of the first detection (which might be rogue).
+
+
+E.g.:
+  %s ../../../../config/config4_db4.yaml 1105142531182852400 --limit 30 --update --ddc
+"""
+
+import sys
+__doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
+from docopt import docopt
+import os, shutil, re, csv, subprocess, io
+
+
+from psat_server_web.atlas.atlas.commonqueries import getLightcurvePoints, getNonDetections, getNonDetectionsUsingATLASFootprint, ATLAS_METADATADDC, filterWhereClauseddc, LC_POINTS_QUERY_ATLAS_DDC, FILTERS
+from makeATLASStamps import doRsync, getObjectsByList, getObjectsByCustomList
+from gkutils.commonutils import dbConnect, PROCESSING_FLAGS, calculateRMSScatter, Struct, cleanOptions, readGenericDataFile, getMJDFromSqlDate
+
+ATLAS_ROOT = '/atlas'
+FORCED_PHOT_DET_SNR = 1.0
+FORCED_PHOT_LIMIT_SNR = 3.0
+
+#FORCED_PHOT_DET_SNR = 3.0
+#FORCED_PHOT_LIMIT_SNR = 3.0
+
+def getForcedPhotometryUniqueExposures(conn, candidateList, discoveryLimit = 20, incremental = True, ddc = False, useFlagDate = False):
+    """getForcedPhotometryUniqueExposures.
+
+    Args:
+        conn:
+        candidateList:
+        discoveryLimit:
+        incremental:
+        ddc:
+        useFlagDate:
+    """
+
+
+    # The algorithm is as follows:
+    # Download all the unique exposures required (multiprocessing)
+    # Download all the unique tphot files required (multiprocessing)
+    # For each object:
+    #    For each exposure:
+    #        grab the forced photometry info from tphorce.
+
+    exposures = []
+    perObjectExposures = {}
+    for candidate in candidateList:
+        existingExposures = []
+        if incremental:
+            # get existing forced photometry
+            fp = getAtlasForcedPhotometry(conn, candidate['id'])
+            existingExposures = [e['expname'] for e in fp]
+
+        objectExps = []
+        objectCoords = []
+        if ddc:
+            p, recurrences = getLightcurvePoints(candidate['id'], lcQuery=LC_POINTS_QUERY_ATLAS_DDC + filterWhereClauseddc(FILTERS), conn = conn)
+        else:
+            p, recurrences = getLightcurvePoints(candidate['id'], conn = conn)
+        # 2017-08-31 KWS Introduced the new ATLAS Footprint based non-detection code.
+        if ddc:
+            b, blanks, lastNonDetection = getNonDetectionsUsingATLASFootprint(recurrences, conn = conn, ndQuery=ATLAS_METADATADDC, filterWhereClause = filterWhereClauseddc, catalogueName = 'atlas_metadataddc')
+        else:
+            b, blanks, lastNonDetection = getNonDetectionsUsingATLASFootprint(recurrences, conn = conn)
+
+        firstDetection = recurrences[0].mjd
+        if useFlagDate:
+            firstDetection = getMJDFromSqlDate(candidate['followup_flag_date'].strftime("%Y-%m-%d") + ' 00:00:00') 
+
+        for row in recurrences:
+            if row.mjd >= firstDetection - discoveryLimit:
+                objectCoords.append({'RA': row.ra, 'DEC': row.dec})
+                if row.expname not in existingExposures:
+                    objectExps.append(row.expname)
+                    exposures.append(row.expname)
+
+        avgRa, avgDec, rms = calculateRMSScatter(objectCoords)
+
+        for row in blanks:
+            if row.mjd >= firstDetection - discoveryLimit:
+                if row.expname not in existingExposures:
+                    objectExps.append(row.expname)
+                    exposures.append(row.expname)
+
+        perObjectExposures[candidate['id']] = {'exps': sorted(list(set(objectExps))), 'avgRa': avgRa, 'avgDec': avgDec}
+
+    uniqueExposures = sorted(list(set(exposures)))
+    return perObjectExposures, uniqueExposures
+
+def doForcedPhotometry(objectList, perObjectExps):
+    """doForcedPhotometry.
+
+    Args:
+        objectList:
+        perObjectExps:
+    """
+    fphot = []
+    for candidate in objectList:
+        #print candidate['id'], perObjectExps[candidate['id']]['avgRa'], perObjectExps[candidate['id']]['avgDec']
+
+        print("Running forced photometry for candidate", candidate['id'])
+
+        #tphorce [options] <diffPath> <tphotOutputPath> <raDeg> <decDeg> <snrLimit>
+        for exp in perObjectExps[candidate['id']]['exps']:
+            camera = exp[0:3]
+            mjd = exp[3:8]
+
+            aux = ''
+            imageName = ATLAS_ROOT + '/diff/' + camera + '/' + mjd + '/' + exp + '.diff.fz'
+
+#            if camera == '02a' and int(mjd) <= 57771:
+#                camera = '02a.ORIG'
+
+            if int(mjd) >= 57350:
+                aux = 'AUX/'
+            tphName = ATLAS_ROOT + '/red/' + camera + '/' + mjd + '/' + aux + exp + '.tph'
+
+            #print TPHORCE, imageName, tphName, perObjectExps[candidate['id']]['avgRa'], perObjectExps[candidate['id']]['avgDec'], '3.0'
+            p = subprocess.Popen([options.tphorce, imageName, tphName, str(perObjectExps[candidate['id']]['avgRa']), str(perObjectExps[candidate['id']]['avgDec']), str(FORCED_PHOT_DET_SNR), str(FORCED_PHOT_LIMIT_SNR)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, errors = p.communicate()
+    
+            if output.strip():
+                csvData = readGenericDataFile(io.StringIO(output), delimiter = ',')
+                # There should only be one CSV row
+                data = None
+                try:
+                    data = csvData[0]
+                except IndexError as e:
+                    print("ERROR: This is not a CSV file. Output = %s" % output.strip())
+
+                if data:
+                    data['limiting_mag'] = False
+
+# {'mjd': '57604.56804193', 'magerr': 'NaN', 'filter': 'c', 'expname': '02a57604o0375c', 'snr': 'NaN', 'mag': 'NaN', 'zp': '22.160', 'id': 1012602110150218500L}
+
+                    if data['mag'] and '>' in data['mag']:
+                        data['mag'] = data['mag'].replace('>','')
+                        data['limiting_mag'] = True
+
+                    data['id'] = candidate['id']
+                    data['expname'] = exp
+                    data['ra'] = perObjectExps[candidate['id']]['avgRa']
+                    data['dec'] = perObjectExps[candidate['id']]['avgDec']
+                    data['snrdet'] = FORCED_PHOT_DET_SNR
+                    data['snrlimit'] = FORCED_PHOT_LIMIT_SNR
+
+                    # Clean up the data - replace 'NaN' with None
+                    for k,v in data.items():
+                        if v == 'NaN':
+                            data[k] = None
+                    fphot.append(data)
+
+            if errors.strip():
+                print(errors)
+
+    return fphot
+
+# 2017-08-07 KWS Added new columns to the forced photometry
+
+def getAtlasForcedPhotometry(conn, objectId):
+    """
+    Get existing forced photometry for a given object
+    """
+
+    import MySQLdb
+    try:
+        cursor = conn.cursor (MySQLdb.cursors.DictCursor)
+
+        cursor.execute ("""
+              SELECT expname,
+                     mjd_obs,
+                     filter,
+                     ra,
+                     `dec`,
+                     mag,
+                     dm,
+                     snr,
+                     zp,
+                     x,
+                     y,
+                     peakval,
+                     skyval,
+                     peakfit,
+                     dpeak,
+                     skyfit,
+                     flux,
+                     dflux,
+                     chin,
+                     major,
+                     minor,
+                     snrdet,
+                     snrlimit,
+                     apfit,
+                     date_inserted,
+                     limiting_mag,
+                     redo_photometry
+              FROM atlas_forced_photometry
+              where atlas_object_id=%s
+              ORDER by mjd_obs
+        """, (objectId,))
+        resultSet = cursor.fetchall ()
+
+        cursor.close ()
+
+    except MySQLdb.Error as e:
+        print("Error %d: %s" % (e.args[0], e.args[1]))
+        sys.exit (1)
+
+
+    return resultSet
+
+def insertForcedPhotometryRow(conn, row):
+    """insertForcedPhotometryRow.
+
+    Args:
+        conn:
+        row:
+    """
+
+    import MySQLdb
+    try:
+        cursor = conn.cursor (MySQLdb.cursors.DictCursor)
+
+        cursor.execute ("""
+             insert into atlas_forced_photometry (atlas_object_id,
+                                                  expname,
+                                                  mjd_obs,
+                                                  ra,
+                                                  `dec`,
+                                                  filter,
+                                                  mag,
+                                                  dm,
+                                                  snr,
+                                                  zp,
+                                                  x,
+                                                  y,
+                                                  peakval,
+                                                  skyval,
+                                                  peakfit,
+                                                  dpeak,
+                                                  skyfit,
+                                                  flux,
+                                                  dflux,
+                                                  chin,
+                                                  major,
+                                                  minor,
+                                                  snrdet,
+                                                  snrlimit,
+                                                  apfit,
+                                                  date_inserted,
+                                                  limiting_mag,
+                                                  redo_photometry)
+             values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, now(), %s, %s)
+             """, (row['id'],
+                   row['expname'],
+                   row['mjd'],
+                   row['ra'],
+                   row['dec'],
+                   row['filter'],
+                   row['mag'],
+                   row['magerr'],
+                   row['snr'],
+                   row['zp'],
+                   row['x'],
+                   row['y'],
+                   row['peakval'],
+                   row['skyval'],
+                   row['peakfit'],
+                   row['dpeak'],
+                   row['skyfit'],
+                   row['flux'],
+                   row['dflux'],
+                   row['chi/N'],
+                   row['major'],
+                   row['minor'],
+                   row['snrdet'],
+                   row['snrlimit'],
+                   row['apfit'],
+                   row['limiting_mag'],
+                   False))
+
+    except MySQLdb.Error as e:
+        sys.stderr.write("Error %d: %s\n" % (e.args[0], e.args[1]))
+
+    cursor.close ()
+    return conn.insert_id()
+
+
+def insertForcedPhotometry(conn, forcedPhotometry):
+    """insertForcedPhotometry.
+
+    Args:
+        conn:
+        forcedPhotometry:
+    """
+    for row in forcedPhotometry:
+        print(row)
+        rowId = insertForcedPhotometryRow(conn, row)
+
+
+# 2016-09-07 KWS Created this to allow exposures to be downloaded via multiprocessing
+def downloadFPExposures(exposureSet):
+    """downloadFPExposures.
+
+    Args:
+        exposureSet:
+    """
+    doRsync(exposureSet, 'diff')
+    # Grab the tphot photometry files
+    doRsync(exposureSet, 'red', getMetadata = True, metadataExtension = '.tph')
+    return exposureSet
+
+
+def main(argv = None):
+
+    """main.
+    """
+    opts = docopt(__doc__, version='0.1')
+    opts = cleanOptions(opts)
+    options = Struct(**opts)
+
+    configFile = options.configfile
+
+    import yaml
+    with open(configFile) as yaml_file:
+        config = yaml.load(yaml_file)
+
+    username = config['databases']['local']['username']
+    password = config['databases']['local']['password']
+    database = config['databases']['local']['database']
+    hostname = config['databases']['local']['hostname']
+
+    detectionList = 1
+    customList = None
+
+    conn = dbConnect(hostname, username, password, database)
+
+    update = options.update
+    limit = int(options.limit)
+
+
+    import yaml
+    with open(configFile) as yaml_file:
+        config = yaml.load(yaml_file)
+
+    username = config['databases']['local']['username']
+    password = config['databases']['local']['password']
+    database = config['databases']['local']['database']
+    hostname = config['databases']['local']['hostname']
+
+    detectionList = 1
+    customList = None
+
+    conn = dbConnect(hostname, username, password, database)
+
+    update = options.update
+    limit = int(options.limit)
+
+    objectList = []
+
+    if options.candidate is not None and len(options.candidate) > 0:
+        for cand in options.candidate:
+            objectList.append({'id': int(cand)})
+    else:
+
+        if options.customlist is not None:
+            if int(options.customlist) > 0 and int(options.customlist) < 100:
+                customList = int(options.customlist)
+                objectList = getObjectsByCustomList(conn, customList)
+            else:
+                print("The list must be between 1 and 100 inclusive.  Exiting.")
+                sys.exit(1)
+        else:
+            if options.detectionlist is not None:
+                if int(options.detectionlist) >= 0 and int(options.detectionlist) < 9:
+                    detectionList = int(options.detectionlist)
+                    objectList = getObjectsByList(conn, listId = detectionList)
+                else:
+                    print("The list must be between 0 and 6 inclusive.  Exiting.")
+                    sys.exit(1)
+
+    print("LENGTH OF OBJECTLIST = ", len(objectList))
+
+
+    conn = dbConnect(hostname, username, password, database)
+
+    perObjectExps, allExps = getForcedPhotometryUniqueExposures(conn, objectList, discoveryLimit = limit, incremental = False, ddc = options.ddc, useFlagDate = options.useflagdate)
+
+    if not options.skipdownload:
+        doRsync(allExps, 'diff')
+        doRsync(allExps, 'red', getMetadata = True, metadataExtension = '.tph')
+
+    fphot = doForcedPhotometry(objectList, perObjectExps)
+
+    insertForcedPhotometry(conn, fphot)
+
+if __name__ == '__main__':
+    main()
---
+++ psat-server/scripts/stamps/python/getATLASForcedPhotometryMultiprocess.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+"""Do ATLAS forced photometry.
+
+Usage:
+  %s <configfile> [<candidate>...] [--detectionlist=<detectionlist>] [--customlist=<customlist>] [--limit=<limit>] [--update] [--ddc] [--skipdownload] [--loglocation=<loglocation>] [--logprefix=<logprefix>] [--loglocationdownloads=<loglocationdownloads>] [--logprefixdownloads=<logprefixdownloads>] [--redregex=<redregex>] [--diffregex=<diffregex>] [--redlocation=<redlocation>] [--difflocation=<difflocation>] [--flagdate=<flagdate>] [--downloadthreads=<downloadthreads>] [--mlscore=<mlscore>]
+  %s (-h | --help)
+  %s --version
+
+Options:
+  -h --help                                       Show this screen.
+  --version                                       Show version.
+  --update                                        Update the database
+  --detectionlist=<detectionlist>                 List option
+  --customlist=<customlist>                       Custom List option
+  --limit=<limit>                                 Number of detections for which to request images [default: 6]
+  --ddc                                           Use the DDC schema for queries
+  --skipdownload                                  Do not attempt to download the exposures (assumes they already exist locally)
+  --loglocation=<loglocation>                     Log file location [default: /tmp/]
+  --logprefix=<logprefix>                         Log prefix [default: forced_photometry]
+  --loglocationdownloads=<loglocationdownloads>   Downloader log file location [default: /tmp/]
+  --logprefixdownloads=<logprefixdownloads>       Downloader log prefix [default: forced_downloader]
+  --redregex=<redregex>                           Reduced image regular expression. Caps = variable. [default: EXPNAME.fits.fz]
+  --diffregex=<diffregex>                         Diff image regular expression. Caps = variable. [default: EXPNAME.diff.fz]
+  --redlocation=<redlocation>                     Reduced image location. E.g. /atlas/diff/CAMERA/fake/MJD.fake (caps = special variable).  Null value means use standard ATLAS archive location.
+  --difflocation=<difflocation>                   Diff image location. E.g. /atlas/diff/CAMERA/fake/MJD.fake (caps = special variable). Null value means use standard ATLAS archive location.
+  --flagdate=<flagdate>                           Date threshold - no hyphens [default: 20151220].
+  --downloadthreads=<downloadthreads>             Number of threads to use for image downloads [default: 10].
+  --mlscore=<mlscore>                             ML score below which we will NOT request forced photometry.
+  --useflagdate                                   Use the flag date as the threshold for the number of days instead of the first detection (which might be rogue).
+
+E.g.:
+  %s ../../../../config/config4_db4.yaml 1105142531182852400 --limit 30 --update --ddc
+
+"""
+
+import sys, os
+__doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
+from docopt import docopt
+import os, shutil, re, csv, subprocess
+
+import datetime
+import subprocess
+from gkutils.commonutils import dbConnect, PROCESSING_FLAGS, Struct, cleanOptions
+import MySQLdb
+from pstamp_utils import REQUESTTYPES
+
+import gc
+from gkutils.commonutils import splitList, parallelProcess
+import queue
+
+from makeATLASStamps import getObjectsByList, getObjectsByCustomList
+from getATLASForcedPhotometry import getForcedPhotometryUniqueExposures, doForcedPhotometry, downloadFPExposures, insertForcedPhotometry
+
+
+def workerExposureDownloader(num, db, listFragment, dateAndTime, firstPass, miscParameters):
+    """thread worker function"""
+    # Redefine the output to be a log file.
+    options = miscParameters[0]
+    sys.stdout = open('%s%s_%s_%d.log' % (options.loglocationdownloads, options.logprefixdownloads, dateAndTime, num), "w")
+
+    # Call the postage stamp downloader
+    objectsForUpdate = downloadFPExposures(listFragment)
+    #q.put(objectsForUpdate)
+    print("Process complete.")
+    return 0
+
+def workerForcedPhotometry(num, db, listFragment, dateAndTime, firstPass, miscParameters, q):
+    """thread worker function"""
+    # Redefine the output to be a log file.
+    options = miscParameters[0]
+    sys.stdout = open('%s%s_%s_%d.log' % (options.loglocation, options.logprefix, dateAndTime, num), "w")
+
+    perObjectExps = miscParameters[1]
+
+    # Call the postage stamp downloader
+    objectsForUpdate = doForcedPhotometry(listFragment, perObjectExps)
+
+    # Write the objects for update onto a Queue object
+    print("Adding %d objects onto the queue." % len(objectsForUpdate))
+
+    q.put(objectsForUpdate)
+
+    print("Process complete.")
+    return 0
+
+
+# ###########################################################################################
+#                                         Main program
+# ###########################################################################################
+
+def main(argv = None):
+    """main.
+
+    Args:
+        argv:
+    """
+
+    opts = docopt(__doc__, version='0.1')
+    opts = cleanOptions(opts)
+    options = Struct(**opts)
+
+    configFile = options.configfile
+
+    import yaml
+    with open(configFile) as yaml_file:
+        config = yaml.load(yaml_file)
+
+    username = config['databases']['local']['username']
+    password = config['databases']['local']['password']
+    database = config['databases']['local']['database']
+    hostname = config['databases']['local']['hostname']
+
+    MAX_NUMBER_OF_OBJECTS = int(config['postage_stamp_parameters']['max_number_of_objects'])
+
+    db = []
+    db.append(username)
+    db.append(password)
+    db.append(database)
+    db.append(hostname)
+
+
+    detectionList = 1
+    customList = None
+
+    conn = dbConnect(hostname, username, password, database)
+
+    update = options.update
+    limit = int(options.limit)
+
+    mlscore = None
+    if options.mlscore is not None:
+        mlscore = float(options.mlscore)
+
+
+    objectList = []
+
+
+    flagDate = '2015-12-20'
+    if options.flagdate is not None:
+        try:
+            flagDate = '%s-%s-%s' % (options.flagdate[0:4], options.flagdate[4:6], options.flagdate[6:8])
+        except:
+            flagDate = '2015-12-20'
+
+    if options.candidate is not None and len(options.candidate) > 0:
+        for cand in options.candidate:
+            objectList.append({'id': int(cand)})
+    else:
+
+        if options.customlist is not None:
+            if int(options.customlist) > 0 and int(options.customlist) < 100:
+                customList = int(options.customlist)
+                objectList = getObjectsByCustomList(conn, customList)
+            else:
+                print("The list must be between 1 and 100 inclusive.  Exiting.")
+                sys.exit(1)
+        else:
+            if options.detectionlist is not None:
+                if int(options.detectionlist) >= 0 and int(options.detectionlist) < 9:
+                    detectionList = int(options.detectionlist)
+                    objectList = getObjectsByList(conn, listId = detectionList, dateThreshold = flagDate)
+                else:
+                    print("The list must be between 0 and 9 inclusive.  Exiting.")
+                    sys.exit(1)
+
+
+    if mlscore is not None and options.candidate is None: # Only do this filter if the IDs are not provided explicitly.
+        updatedList = []
+        for row in objectList:
+            if row['zooniverse_score'] is not None and row['zooniverse_score'] >= mlscore:
+                updatedList.append(row)
+        if len(updatedList) > 0:
+            objectList = updatedList
+
+    currentDate = datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")
+    (year, month, day, hour, min, sec) = currentDate.split(':')
+    dateAndTime = "%s%s%s_%s%s%s" % (year, month, day, hour, min, sec)
+
+    # Single threaded
+    #perObjectExps, exposureSet = getForcedPhotometryUniqueExposures(conn, objectList, discoveryLimit = limit, ddc = options.ddc, useFlagDate = options.useflagdate)
+    perObjectExps, exposureSet = getForcedPhotometryUniqueExposures(conn, objectList, discoveryLimit = limit, ddc = options.ddc, useFlagDate = True)
+    # We'll hand the entire perObjectExps dictionary to each thread.
+
+
+    # Download threads with multiprocessing - try 10 threads by default
+    print("TOTAL OBJECTS = %d" % len(exposureSet))
+
+    print("Downloading exposures...")
+
+    if not options.skipdownload:
+        if len(exposureSet) > 0:
+            nProcessors, listChunks = splitList(exposureSet, bins = options.downloadthreads)
+
+            print("%s Parallel Processing..." % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+            parallelProcess(db, dateAndTime, nProcessors, listChunks, workerExposureDownloader, miscParameters = [options], drainQueues = False)
+            print("%s Done Parallel Processing" % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+
+            # Belt and braces - try again with one less thread, just in case the previous one failed.
+            nProcessors, listChunks = splitList(exposureSet, bins = options.downloadthreads - 1)
+
+            print("%s Parallel Processing..." % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+            parallelProcess(db, dateAndTime, nProcessors, listChunks, workerExposureDownloader, miscParameters = [options], drainQueues = False)
+            print("%s Done Parallel Processing" % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+
+    # Produce stamps with multiprocessing - try n(CPUs) threads by default
+    print("Doing Forced Photometry...")
+
+    if len(objectList) > 0:
+        nProcessors, listChunks = splitList(objectList)
+
+        print("%s Parallel Processing..." % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+        objectsForUpdate = parallelProcess(db, dateAndTime, nProcessors, listChunks, workerForcedPhotometry, miscParameters = [options, perObjectExps])
+        print("%s Done Parallel Processing" % (datetime.datetime.now().strftime("%Y:%m:%d:%H:%M:%S")))
+        if len(objectsForUpdate) > 0 and update:
+            insertForcedPhotometry(conn, objectsForUpdate)
+
+    conn.close()
+
+    return 0
+
+
+
+if __name__ == '__main__':
+    main()
+    
