a1af744d93142115342297c2b7713364262a9dae
Ken Smith <k.w.smith@qub.ac.uk>
2024-03-07 17:46:17 +0000

    Combined dets and nondet info, and use newly created date_inserted column.

--- psat-server/scripts/utils/python/updateVRAScores.py
+++ psat-server/scripts/utils/python/updateVRAScores.py
@@ -40,7 +40,7 @@ import sys
 __doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
 from docopt import docopt
 import os, shutil, re, csv, subprocess
-from gkutils.commonutils import Struct, cleanOptions, dbConnect, coords_dec_to_sex, getDateFractionMJD, readGenericDataFile
+from gkutils.commonutils import Struct, cleanOptions, dbConnect, coords_dec_to_sex, getDateFractionMJD, readGenericDataFile, getMJDFromSqlDate
 
 # 2024-03-05 KWS Need to add Heloise's code into the pythonpath.
 from st3ph3n.api_utils import atlas as atlasapi
@@ -50,6 +50,7 @@ import json
 import random
 import yaml
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 import pkg_resources
 
@@ -65,12 +66,52 @@ def runUpdates(options):
       - output the list of ATLAS 19 digit IDs and timestamps (in pairs).
     """
 
-    date_threshold = (datetime.now() - timedelta(days=float(options.ndays))).strftime("%Y-%m-%d")
+    date_threshold = (datetime.now() - timedelta(days=float(options.ndays))).strftime("%Y-%m-%d %H:%M:%S")
     payload = {'datethreshold': date_threshold}
-    request_vra_scores = atlasapi.RequestVRAScores(api_config_file=api_config, payload = payload)
-    request_vra_scores.get_response()
+    request_vra_scores = atlasapi.RequestVRAScores(api_config_file=api_config,
+                                                   payload = payload,
+                                                   get_response = True
+                                                   )
     vra_df = pd.DataFrame(request_vra_scores.response)
-    print(vra_df.head())
+    unique_transient_ids = set(vra_df.transient_object_id.values)
+    transient_ids_with_decisions = set(vra_df[~vra_df.username.isnull()].transient_object_id.values)
+    set_to_update = unique_transient_ids - transient_ids_with_decisions
+    print(vra_df[np.isin(vra_df.transient_object_id.values, list(set_to_update))][['transient_object_id','timestamp']].drop_duplicates(subset='transient_object_id', keep='last'))
+
+    request_data = atlasapi.RequestMultipleSourceData(api_config_file=api_config,
+                                                     array_ids=np.array(list(set_to_update))[:2],
+                                                     mjdthreshold = getMJDFromSqlDate(date_threshold)
+                                                     )
+    request_data.chunk_get_response_quiet()
+
+    first_response = request_data.response[0]
+    data_lc = pd.DataFrame(first_response['lc'])
+    data_lcnondets = pd.DataFrame(first_response['lcnondets'])
+    data_lcnondets['magerr'] = 0.0
+
+    data_lc = data_lc[['mjd','filter','mag','magerr','date_inserted']]
+    data_lc['det'] = 1
+    data_lcnondets = data_lcnondets[['mjd','filter','mag5sig','magerr','date_inserted']]
+    data_lcnondets.columns=['mjd','filter','mag','magerr','date_inserted']
+    data_lcnondets['det'] = 0
+    data_all = pd.concat([data_lc, data_lcnondets]).sort_values('mjd').reset_index(drop=True)
+    print(data_all.head())
+
+    #for 
+    #if no recent data:
+    #   Do nowt.
+    #if recent detection:
+    #   preal = 1
+    #elif recent non-detection and non-detection (mag5sig) fainter than previous detection
+    #   preal = 0
+    #elif recent non-detection and non-detection brighter than previous detection
+    #   new row preal unchanged, new timestamp
+
+
+
+
+    first_object = request_data.response[0]
+    print(len(first_object['lc']), len(first_object['lcnondets']))
 
 
 def main():
