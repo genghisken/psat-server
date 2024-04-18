fad69a6d18ac738599f8c484a32001837a99e9da
Ken Smith <k.w.smith@qub.ac.uk>
2024-04-18 20:06:55 +0100

    Updated joinDDCwithNNC.sh to do checking of files existing, etc. Added generateATLASSchemaAndCPPClasses.py

--- psat-server/scripts/utils/bash/joinDDCwithNNC.sh
+++ psat-server/scripts/utils/bash/joinDDCwithNNC.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# Rename a database from one schema to a new EMPTY schema. This does NOT (and cannot) move the views.
-# Recreate the views on the new database AFTER the move. The user must have full permissions on both schemas.
+# Join the ddc and nnc files together for ingest into the database.
 
 if [ $# -ne 1 ]
 then
-   echo "Usage: `basename $0` <ddcfile>"
-   exit 1
+    echo "Usage: `basename $0` <ddcfile>"
+    exit 1
 fi
-
-# Check that ddc.nnc file does NOT exist. Abort if already exists.
-# Check that there is BOTH a ddc file AND an nnc file (otherwise abort)
-# If possible, skip duplicate columns in nnc file, but the duplicate columns will be stored in the nnc table.
 
 pid=$$
 ddcFile=$1
 expnameFilePrefix=${ddcFile%.ddc}
 nncFile=${expnameFilePrefix}.nnc
-colmerge=/atlas/bin/cm
 
-# 2016-03-22 KWS Complete rewrite required.  We CANNOT use unix sorting unless both values of x
-#                and y in both files are identical.  If one has more decimal places, the sorting
-#                breaks down.  Use JT colmerge code instead and grep -v the duplicates.
+# Check that there is BOTH a ddc file AND an nnc file (otherwise abort)
+if ! [ -e "$ddcFile" ] || ! [ -e "$nncFile" ]
+then
+    echo "BOTH ddc and nnc files must exist. Aborting."
+    exit 1
+fi
 
-# 2017-02-10 KWS John has moved the moments files to AUX from 57793 onwards!
+# Check that ddc.nnc file does NOT exist. Abort if already exists.
+if [[ -e ${expnameFilePrefix}.ddc.nnc ]]
+then
+    echo "Already have a ddc.nnc file. Aborting."
+    exit 1
+fi
+
+# If possible, skip duplicate columns in nnc file, but the duplicate columns will be stored in the nnc table.
+
+colmerge=$(which cm)
+
+if [[ -z $colmerge ]]
+then
+    echo "Cannot find cm. Aborting."
+    exit 1
+fi
+
 ddcBasename=$(basename "$ddcFile")
 ddcDirname=$(dirname "$ddcFile")
 mjd=${ddcBasename:3:5}
@@ -33,6 +46,6 @@ grep "^#" $ddcFile | sed '$d' > ${expnameFilePrefix}.ddc.nnc
 
 # Now grab the column headers in each file and paste them together
 ddccolumns=`grep "^#" $ddcFile | tail -1`
-nnccolumns=`head -1 $nncFile | tr '#' ' '`
+nnccolumns=`head -1 $nncFile | tr '#' ' ' | sed -e 's/ \{1,\}/ nnc_/g'`
 echo "$ddccolumns $nnccolumns" >> ${expnameFilePrefix}.ddc.nnc
 $colmerge 5,6,10 $ddcFile 5,6,10 $nncFile | grep -v ' D ' | tr '|' ' ' >> ${expnameFilePrefix}.ddc.nnc
