3ed2aefb07e82c604e1b12fbfe89f4b214cf1545
Ken Smith <k.w.smith@qub.ac.uk>
2024-06-12 12:06:33 +0100

    Removed extra train information from nnc header

--- psat-server/scripts/utils/bash/joinDDCwithNNC.sh
+++ psat-server/scripts/utils/bash/joinDDCwithNNC.sh
@@ -20,10 +20,10 @@ then
     exit 1
 fi
 
-# Check that ddc.nnc file does NOT exist. Abort if already exists.
-if [[ -e ${expnameFilePrefix}.ddc.nnc ]]
+# Check that dnc file does NOT exist. Abort if already exists.
+if [[ -e ${expnameFilePrefix}.dnc ]]
 then
-    echo "Already have a ddc.nnc file. Aborting."
+    echo "Already have a dnc file. Aborting."
     exit 1
 fi
 
@@ -42,10 +42,10 @@ ddcDirname=$(dirname "$ddcFile")
 mjd=${ddcBasename:3:5}
 
 # Grab the headers. First the main ddc header.
-grep "^#" $ddcFile | sed '$d' > ${expnameFilePrefix}.ddc.nnc
+grep "^#" $ddcFile | sed '$d' > ${expnameFilePrefix}.dnc
 
 # Now grab the column headers in each file and paste them together
 ddccolumns=`grep "^#" $ddcFile | tail -1`
-nnccolumns=`head -1 $nncFile | tr '#' ' ' | sed -e 's/ \{1,\}/ nnc_/g'`
-echo "$ddccolumns $nnccolumns" >> ${expnameFilePrefix}.ddc.nnc
-$colmerge 5,6,10 $ddcFile 5,6,10 $nncFile | grep -v ' D ' | tr '|' ' ' >> ${expnameFilePrefix}.ddc.nnc
+nnccolumns=`head -1 $nncFile | sed -e 's/ train.*//' | tr '#' ' ' | sed -e 's/ \{1,\}/ nnc_/g'`
+echo "$ddccolumns $nnccolumns" >> ${expnameFilePrefix}.dnc
+$colmerge 5,6,10 $ddcFile 5,6,10 $nncFile | grep -v ' D ' | tr '|' ' ' >> ${expnameFilePrefix}.dnc
