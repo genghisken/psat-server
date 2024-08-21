24bd09489f8e59c4e02fe633b9ec0262c1b19d63
Ken Smith <k.w.smith@qub.ac.uk>
2024-08-21 16:56:06 +0000

    Added new VRA Garbage Collector python and wrapper script.

---
+++ psat-server/scripts/utils/bash/VRAGarbageCollector.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#  VRA garbage collector wrapper script.
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: `basename $0` <code root> <config file>"
+    exit 1
+fi
+
+export CODEROOT=$1
+export CONFIGFILE=$2
+
+# We need to specify $CODEROOT, because the code needs to be able to find
+# the parse_yaml.sh common code in order to read the yaml file!
+if [[ ! -f "$CODEROOT/scripts/utils/bash/parse_yaml.sh" ]]
+then
+    echo "Cannot find the YAML parser ($CODEROOT/scripts/utils/bash/parse_yaml.sh)"
+    exit
+fi
+
+. $CODEROOT/scripts/utils/bash/parse_yaml.sh
+eval $(parse_yaml $CONFIGFILE)
+
+export DBUSER=$databases__local__username
+export DBPASS=$databases__local__password
+export DBHOST=$databases__local__hostname
+export DBNAME=$databases__local__database
+export CODEBASE=$code_paths__codebase
+export LOGLOCATION=$code_paths__loglocation
+export LOGPREFIX=$code_paths__logprefix
+
+# Belt & braces! If one of the variables above is not set, don't bother continuing.
+if [[ ! $DBNAME ]]
+then
+    echo Cannot find the config file. Aborting.
+    exit
+fi
+
+# equivalent to conda activate vra
+export PYTHONVRA=/usr/local/swtools/python/atls/anaconda3/envs/vra/bin/python
+export PYTHONPATH=/usr/local/ps1code/gitrelease/stephen/st3ph3n:/usr/local/ps1code/gitrelease/atlasapiclient
+export CONFIG_ATLASAPI=/usr/local/ps1code/gitrelease/atlasapiclient/atlasapiclient/config_files/api_config_MINE.yaml
+
+export OUTPUT_FILE=/$DBHOST/$LOGLOCATION/garbage_`date +%Y%m%d_%H%M`_vra.log
+
+$PYTHONVRA /usr/local/ps1code/gitrelease/psat-server/psat-server/scripts/utils/python/VRAGarbageCollector.py $CONFIG_ATLASAPI $OUTPUT_FILE --debug
+
+#export OUTPUT_FILE=/$DBHOST/$LOGLOCATION/garbage_20240821_1517_vra.log
+
+export ids=`cat $OUTPUT_FILE | tr '\n' ',' | sed -e 's/,$//'`
+export sqldelete="update atlas_diff_objects set detection_list_id=0, local_comments='VRA Garbage Collection', date_modified=now() where id in ($ids);"
+
+echo "Moving retired objects to the garbage list."
+mysql -u$DBUSER --password="$DBPASS" $DBNAME -h$DBHOST -Be "$sqldelete"
+echo "Done."
+
---
+++ psat-server/scripts/utils/python/VRAGarbageCollector.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+"""VRA garbage collector
+
+Usage:
+  %s <apiConfigFile> <outputFile> [--debug] 
+  %s (-h | --help)
+  %s --version
+
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+  --debug                           Debug mode.
+  --rbthreshold=<rbthreshold>       RB Threshold (if not set will be ignored).
+
+E.g.:
+  %s ../../../../../atlas/config/api_config_file.yaml /tmp/object_to_delete.csv
+"""
+
+import sys
+__doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
+from docopt import docopt
+import os, shutil, re, csv, subprocess
+from gkutils.commonutils import Struct, cleanOptions, dbConnect, coords_dec_to_sex, getDateFractionMJD, readGenericDataFile
+
+import requests
+import json
+
+import pandas as pd
+import numpy as np
+
+
+# 2024-06-24 KWS Use the st3ph3n API code to write the results.
+from st3ph3n.utils import api as vraapi
+from atlasapiclient import client as atlasapi
+
+def insertVRAEntry(API_CONFIG_FILE, objectId, pReal, pGal, rank, debug = False):
+    payload = {'objectid': objectId, 'preal': pReal, 'pgal': pGal, 'rank': rank, 'debug': debug}
+    writeto_vra = vraapi.WriteToVRAScores(api_config_file = API_CONFIG_FILE, payload=payload)
+    writeto_vra.get_response()
+    
+    
+def get_vra_eyeball():
+    todo_list = atlasapi.RequestVRAToDoList(payload = {'datethreshold': "2024-02-22"}, get_response=True)
+    todo_df=pd.DataFrame(todo_list.response)
+    vra_df = vraapi.fetch_vra_dataframe(datethreshold=todo_df.timestamp.min()).set_index('transient_object_id')
+    return vra_df.loc[todo_df.transient_object_id.values]
+
+
+def main():
+    """main.
+    """
+    opts = docopt(__doc__, version='0.1')
+    opts = cleanOptions(opts)
+    options = Struct(**opts)
+
+    debug = options.debug
+    if debug is None:
+        debug = False
+
+    configFile = options.apiConfigFile
+    outputFile = options.outputFile
+    
+    vra_eyeball_allrows = get_vra_eyeball()
+    vra_df_eyeball= vra_eyeball_allrows.reset_index().drop_duplicates('transient_object_id', keep='last')
+
+
+
+    # The delete conditions are based on the maximum VRA rank an object has received
+    # and how many updates an object has received
+    max_ranks = vra_eyeball_allrows.reset_index().groupby('transient_object_id')['rank'].max()
+    counts = vra_eyeball_allrows.reset_index().groupby('transient_object_id').count().id.rename('counts')
+    counts_n_ranks = pd.concat((max_ranks, counts), axis=1)
+
+
+    # The first condition is:
+    # If an object has had at eadt one update and it's rasnk never rose asbove 3
+    ids_to_del_1 = counts_n_ranks[ (counts_n_ranks.counts>1)
+                   &(counts_n_ranks['rank']<3)
+                  ].index.values
+
+    # The second condition is 
+    # If an oject was given a rank less than 1.5 delete (even if seen only once)
+    ids_to_del_2 = set(counts_n_ranks[ (counts_n_ranks.counts>=1)
+                   &(counts_n_ranks['rank']<1.5)
+                  ].index.values) 
+
+    list_to_delete = list(set(ids_to_del_1).union(set(ids_to_del_2)))
+    delete_df = pd.DataFrame([list_to_delete]).T
+    # save the atlas ids to delete to a text file
+    delete_df.to_csv(outputFile, header=False, index=False)
+    
+    # add a row in the vra scores table to record the deletion
+    for atlas_id in list_to_delete:
+        insertVRAEntry(configFile, atlas_id, None, None, -1, debug = debug)
+        
+if __name__ == '__main__':
+    main()
