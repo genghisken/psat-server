d1e4b9630f31b1cc557f9540a17995b90d8357d9
Ken Smith <k.w.smith@qub.ac.uk>
2026-08-24 21:10:21 +0100

    Bug fixes for panstarrsTNSDaemon.py.

--- psat-server/scripts/tns/python/panstarrsTNSDaemon.py
+++ psat-server/scripts/tns/python/panstarrsTNSDaemon.py
@@ -141,7 +141,7 @@ def listen(options):
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((server, port))
+        s.bind((options.server, int(options.port)))
         s.listen(1)
 
         while True:
