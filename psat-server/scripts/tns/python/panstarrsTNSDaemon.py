97778e09448f97553ef272f6b4a79e402aa0ccd9
Ken Smith <k.w.smith@qub.ac.uk>
2026-08-25 11:05:50 +0100

    Fixed the missing options.internalids problem.

--- psat-server/scripts/tns/python/panstarrsTNSDaemon.py
+++ psat-server/scripts/tns/python/panstarrsTNSDaemon.py
@@ -191,7 +191,7 @@ def listen(options):
                                 zooniverseScoreThreshold=zooniverseScoreThreshold,
                                 botId=botId,
                                 botName=botName,
-                                addInternalIDs=internalids,
+                                addInternalIDs=options.internalids,
                             )
                             for row in reports:
                                 logger.info('Daemon Submission - TNS report ID = %s', row)
@@ -237,7 +237,7 @@ def listen(options):
                                 zooniverseScoreThreshold=zooniverseScoreThreshold,
                                 botId=botId,
                                 botName=botName,
-                                addInternalIDs=internalids,
+                                addInternalIDs=options.internalids,
                             )
                             for row in reports:
                                 logger.info('Daemon TEST Submission - TNS report ID = %s', row)
